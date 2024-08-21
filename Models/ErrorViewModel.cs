010263d3713f4a8ecb46ee5022f2e14f62099e1b
Santiagopt97 <santiagopineda99@gmail.com>
2024-08-21 07:27:26 -0500

    Feat: Creacion del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Biblioteca_c_sharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
