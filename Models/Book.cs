51c4b20a0365f422bc3d2d275e37b5a810c91132
Deilons <apolo4748@gmail.com>
2024-08-21 08:20:32 -0500

    Feat: atributos de la clase Book

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 
-namespace Biblioteca_c_sharp.Models
+namespace Biblioteca_c_sharp.Models;
+
+public class Book
 {
-    public class Book
-    {
-        
-    }
+    public BigInteger Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public string Isbn { get; set; }
+
+    public string Category { get; set; }
+
+    public bool Available { get; set; }
 }
