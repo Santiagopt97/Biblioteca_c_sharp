cabca8c84c5af7260374f5977a125dfb88794f58
Camilobarre <barrecami@gmail.com>
2024-08-21 19:47:18 -0500

    feat: Se crean las vistas del CRUD

---
+++ Models/User.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Biblioteca_c_sharp.Models;
+public class User
+{
+    [Key]
+    public BigInteger Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public string Isbn { get; set; }
+
+    public string Category { get; set; }
+
+    [Required]
+
+    public bool Available { get; set; }
+
+    public User(BigInteger id, string title, string author, string isbn, string category, bool available)
+    {
+        Id = id;
+        Title = title;
+        Author = author;
+        Isbn = isbn;
+        Category = category;
+        Available = available;
+    }
+}
